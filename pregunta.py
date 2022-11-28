17d2fe834b7810914d26b791aed399ff74390875
wgarciab <55595765+wgarciab@users.noreply.github.com>
2022-11-28 15:50:16 -0500

    Added solution

--- pregunta.py
+++ pregunta.py
@@ -17,4 +17,32 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    df = df.dropna()
+
+    df_str_columns = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'barrio', 'línea_credito']
+
+    for column in df_str_columns:
+        df[column] = df[column].str.lower()
+        df[column] = df[column].str.replace('-', ' ')
+        df[column] = df[column].str.replace('_', ' ')
+
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype('int64')
+
+    df['fecha_de_beneficio'] = pd.to_datetime(
+        df['fecha_de_beneficio'],
+        infer_datetime_format=True, 
+        errors='ignore',
+        dayfirst=True,
+    )
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime("%Y/%m/%d")
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip('$')
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '')
+    df['monto_del_credito'] = df['monto_del_credito'].astype('float64')
+
+
+    subset=df.columns.values.tolist()
+    subset.remove('Unnamed: 0')
+    df.drop_duplicates(subset=subset, inplace=True)
+
     return df
